6147809d4425348dafc365a31217826e3340c5ce
PENLAP <penlaptj@3il.fr>
2024-11-26 16:03:48 +0100

    Modification exercice2

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,45 @@
+/**
+ * Affiche notes et moyennes
+ */
 public class GestionnaireNotes {
 
-    // Affiche note et moyennes
+    
     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
         // Affichage des notes
-        System.out.println("Notes de " + nomEtudiant + ":");
+        afficheNotes(nomEtudiant);
+        // Calcul de la somme des notes
+        int somme=calculSommeNotes(notes);
+        // Affichage de la moyenne
+        afficheMoyenne(somme,notes);
+    }
+}
+
+/**
+ * Affiche notes 
+ * */
+public void afficheNotes(nomEtudiant) {
+    System.out.println("Notes de " + nomEtudiant + ":");
         for (int note : notes) {
             System.out.println("- " + note);
         }
+}
 
-        // Calcul de la somme des notes
-        int somme = 0;
+/**
+ * Calcul somme des notes
+ * @return: la sommes des notes
+ */
+public int calculSommeNotes(int[] notes) {
+    int somme = 0;
         for (int note : notes) {
             somme += note;
         }
+    return somme;
+}
 
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
-    }
+/**
+ * Affiche moyennes
+ */
+public void afficheMoyenne(int somme,int notes) {
+    double moyenne = (double) somme / notes.length;
+    System.out.println("Moyenne : " + moyenne);
 }
